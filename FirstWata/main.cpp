bceccd7fce540c90b75af5bfc89f6a0aafab7754
OFedorov <offedoroff@yandex.ru>
2016-04-02 03:50:57 +0800

    Hello, Valentin!

---
+++ FirstWata/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "My congratulations!" << endl;
+    cout << "You have got to use git." << endl;
+    cout << "Now you must do you first commit." << endl;
+    cout << "Good Luck!" << endl;
+    return 0;
+}
+
